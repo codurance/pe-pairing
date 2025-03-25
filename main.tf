73848fac09de96e9caead113a3b72db36ae0cd84
Toby <tobywoollaston@hotmail.co.uk>
2025-03-25 15:45:17 +0000

    create an s3 bucket

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-unique-bucket-name"  # Must be globally unique across all AWS accounts
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
