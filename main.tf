b5906ccad7bf4a56a7d9298df82c2de084f3bfda
Toby <tobywoollaston@hotmail.co.uk>
2025-03-25 15:47:15 +0000

    maybe try this

--- main.tf
+++ main.tf
@@ -15,11 +15,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-unique-bucket-name"  # Must be globally unique across all AWS accounts
-
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "this" {
+  bucket = "the_bucket_name"
 }
