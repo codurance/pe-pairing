90abfaece18215e0ea2a41837ddfefc78012d296
Toby <tobywoollaston@hotmail.co.uk>
2025-03-25 15:55:10 +0000

    retry rename again

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "the-bucket-name"
+resource "aws_s3_bucket" "this" {
+  bucket = "pe-pairing-bucket-name-1387321"
 }
