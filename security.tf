8a7db4f6dcd9be2ee19fd819b4f17e4a73ac26c3
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2025-04-01 18:39:35 +0100

    Allo HTTP access to the VM

--- security.tf
+++ security.tf
@@ -16,6 +16,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.main.id
+  description       = "Allow HTTP inbound traffic"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
 resource "aws_vpc_security_group_egress_rule" "all_egress" {
   security_group_id = aws_security_group.main.id
   description       = "Allow all egress"
