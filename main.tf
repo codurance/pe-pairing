e982836c38334625e6d3af79806ff95571c900e5
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2025-04-01 17:25:20 +0100

    Create a VPC

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  backend "s3" {
-    # These key and value pairs are passed with -backend-config
-  }
+variable "env" {
+  type    = string
+  default = "ivan-katzarski"
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "eu-west-2"
+resource "aws_vpc" "main" {
+  cidr_block           = "10.100.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
 }
---
+++ provider.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    # These key and value pairs are passed with -backend-config
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
