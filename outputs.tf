2f7c63f3ac85cb1ecf215e112be65df2ad2619cf
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2025-04-01 18:23:29 +0100

    Mark the private key output as sensitive

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,5 @@ output "nginx_instance_public_ip" {
 output "private_key" {
   description = "The private key needed to SSH into the NGINX instance."
   value       = tls_private_key.main.private_key_pem
-  # sensitive   = true
+  sensitive   = true
 }
