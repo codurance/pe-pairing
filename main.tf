d556a46657d6e354868d2b25a9403c0750eec05b
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2025-04-01 17:42:38 +0100

    Create public route table and internet gateway

--- main.tf
+++ main.tf
@@ -20,3 +20,24 @@ resource "aws_subnet" "public" {
     Name = "${var.env}-public-subnet"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-route-table"
+  }
+}
