f12a101c240da5b57d2b5d32c2b6cb4ca544d9aa
Henrique <henriqueos98@outlook.com>
2025-05-02 16:41:35 -0300

    validate correction

--- ec2.tf
+++ ec2.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.public.id
 }
 
